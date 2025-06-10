030567b55b2e189290df8597d8dac20fbe92111a
Birdman1104 <arsenmazmanyan11@gmail.com>
2025-06-11 01:26:00 +0400

    vite fix.

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  base: "",
+  base: "/evosolar-website/",
   server: {
     hmr: {
       // overlay: false, // Disable error overlay if it gets annoying
